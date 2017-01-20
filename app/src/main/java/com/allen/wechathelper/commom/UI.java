7f63ec3b9c0981b9dcdc5606d29bcdc2234978b2
allen <lygttpod@163.com>
2017-01-20 17:46:05 +0800

    更新开启红包的ID

--- app/src/main/java/com/allen/wechathelper/commom/UI.java
+++ app/src/main/java/com/allen/wechathelper/commom/UI.java
@@ -8,5 +8,5 @@ public class UI {
     public final static String EDITTEXT = "android.widget.EditText";
     public final static String LUCKY_MONEY_RECEIVE_UI = "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI";
     public final static String LUCKY_MONEY_DETAIL_UI = "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI";
-    public final static String OPEN_LUCKY_MONEY_BUTTON_ID = "com.tencent.mm:id/bag";
+    public final static String OPEN_LUCKY_MONEY_BUTTON_ID = "com.tencent.mm:id/be_";
 }
