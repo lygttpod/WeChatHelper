b43df02170959b70aac9ca9713402d2b68d1b58d
xiaoyao <xiaoyao@51vest.com>
2018-09-25 17:29:25 +0800

    更新微信红包按钮的ID

--- app/src/main/java/com/allen/wechathelper/commom/UI.java
+++ app/src/main/java/com/allen/wechathelper/commom/UI.java
@@ -4,9 +4,11 @@ package com.allen.wechathelper.commom;
  * Created by allen on 2016/9/18.
  */
 public class UI {
-    public final static String BUTTON = "android.widget.Button";
-    public final static String EDITTEXT = "android.widget.EditText";
+
     public final static String LUCKY_MONEY_RECEIVE_UI = "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI";
     public final static String LUCKY_MONEY_DETAIL_UI = "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI";
-    public final static String OPEN_LUCKY_MONEY_BUTTON_ID = "com.tencent.mm:id/be_";
+    /**
+     * id微信为了防止作弊每个版本都会修改，在使用之前可以通过ADM(Android Device Monitor)查看最新点击按钮的id即可
+     */
+    public final static String OPEN_LUCKY_MONEY_BUTTON_ID = "com.tencent.mm:id/cb1";
 }
